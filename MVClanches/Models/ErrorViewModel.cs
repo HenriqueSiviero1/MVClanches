32a491dfef2724902b7a06a783c0ba13d19c3d52
HenriqueSiviero1 <henriquesiviero28@gmail.com>
2023-05-10 15:09:49 -0300

    projeto MVClanches

---
+++ MVClanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVClanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
